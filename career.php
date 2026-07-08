6576710f18fb95fa24875f1b25af913018b1f38b
Zylow756 <zylow0744@gmail.com>
2026-07-08 18:25:06 +0530

    changing the path of config file in local directory

--- career.php
+++ career.php
@@ -1,12 +1,8 @@
 <?php
-require_once __DIR__ . "/../config.php";
+require_once __DIR__ . "/config.php";
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
-}
-if (empty($_SESSION['user'])) {
-    header("Location: index.php?r=0");
-    exit;
 }
 
 require_once "thecaptcha/captcha.function.php";
