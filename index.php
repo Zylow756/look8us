04684869589cf68e90db4f45e5f6ff30fdcbc570
Zylow756 <zylow0744@gmail.com>
2026-07-17 19:09:17 +0530

    edit part 2

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-header("Location: home.php");
-exit;
-?>
-
-<?php
 declare(strict_types=1);
 require_once __DIR__ . '/config.php';
+
+header("Location: home.php");
+exit;
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
